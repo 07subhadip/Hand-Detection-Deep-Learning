6dadc32f28bae8ebd0661157371a904dae645098
07subhadip <91666506+07subhadip@users.noreply.github.com>
2024-04-06 10:57:20 +0530

    commit

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -4,8 +4,34 @@ import time
 
 cap = cv2.VideoCapture(0)
 
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+pTime = 0
+cTime = 0
+
 while True:
     success , img = cap.read()
+    imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    result = hands.process(imgRGB)
+    #print(result.multi_hand_landmarks)
+
+    if result.multi_hand_landmarks:
+        for handLMS in result.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, handLMS , mpHands.HAND_CONNECTIONS)
+    
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv2.putText(img,str(int(fps)),(10,70),cv2.FONT_HERSHEY_COMPLEX,2,(255,0,255),3)
+
+
     cv2.imshow("Image",img)
-    cv2.waitKey(1)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 # this is to access the camera of device or any cam module like webcam
+
+cap.release()
+cv2.destroyAllWindows()
